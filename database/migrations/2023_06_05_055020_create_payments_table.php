a93dba2684323a2f191a107051a1e769b53ee004
ali sabir <iali41462@gmail.com>
2026-08-16 15:55:55 +0500

    Fix payment enrollment foreign key

--- database/migrations/2023_06_05_055020_create_payments_table.php
+++ database/migrations/2023_06_05_055020_create_payments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            // $table->unsignedBigInteger('enrollment_id');
+            $table->unsignedBigInteger('enrollment_id');
             $table->foreign('enrollment_id')
       ->references('id')
       ->on('enrollments')
