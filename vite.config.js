d0d28689666cac0e00b581e5a8e665ee1d87f6c9
ali sabir <iali41462@gmail.com>
2026-08-16 20:36:59 +0500

    Fix React Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.jsx'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
+        react(),
     ],
 });
